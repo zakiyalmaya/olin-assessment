8e9b0e9a2b8c5011b9045243b90e6c5e7779bdf2
Zakiya Almaya <zakiya.almaya@gmail.com>
2024-05-09 11:05:22 +0700

    Create new handler for Question 2&3

--- transport/controller/golang_assessment/controller.go
+++ transport/controller/golang_assessment/controller.go
@@ -23,6 +23,11 @@ func (c *Controller) Question1(w http.ResponseWriter, r *http.Request) {
 	numsStr := r.URL.Query().Get("nums")
 	targetStr := r.URL.Query().Get("target")
 
+	if numsStr == "" || targetStr == "" {
+		response.RespondWithError(w, http.StatusBadRequest, "invalid request")
+		return
+	}
+
 	numsArrStr := strings.Split(numsStr, ",")
 	nums := make([]int, len(numsArrStr))
 	for i, numStr := range numsArrStr {
@@ -42,5 +47,43 @@ func (c *Controller) Question1(w http.ResponseWriter, r *http.Request) {
 	}
 
 	result := c.golangAssessmentSvc.Question1(nums, target)
+	response.RespondWithJSON(w, http.StatusOK, result)
+}
+
+func (c *Controller) Question2(w http.ResponseWriter, r *http.Request) {
+	numsStr := r.URL.Query().Get("nums")
+
+	if numsStr == "" {
+		response.RespondWithError(w, http.StatusBadRequest, "invalid request")
+		return
+	}
+
+	numsArrStr := strings.Split(numsStr, ",")
+	nums := make([]int, len(numsArrStr))
+	for i, numStr := range numsArrStr {
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			response.RespondWithError(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		nums[i] = num
+	}
+
+	result := c.golangAssessmentSvc.Question2(nums)
+	response.RespondWithJSON(w, http.StatusOK, result)
+}
+
+func (c *Controller) Question3(w http.ResponseWriter, r *http.Request) {
+	sentence := r.URL.Query().Get("sentence")
+	words := r.URL.Query().Get("words")
+	wordsArr := strings.Split(words, ",")
+
+	if sentence == "" || words == "" {
+		response.RespondWithError(w, http.StatusBadRequest, "invalid request")
+		return
+	}
+
+	result := c.golangAssessmentSvc.Question3(sentence, wordsArr)
 	response.RespondWithJSON(w, http.StatusOK, result)
 }
--- transport/handler.go
+++ transport/handler.go
@@ -10,4 +10,6 @@ func Handler(golangAssessmentSvc golangSvc.Service, r *mux.Router) {
 	ctrl := controller.NewController(golangAssessmentSvc)
 
 	r.HandleFunc("/golang/question1", ctrl.GolangAssessmentCtrl.Question1).Methods("GET")
+	r.HandleFunc("/golang/question2", ctrl.GolangAssessmentCtrl.Question2).Methods("GET")
+	r.HandleFunc("/golang/question3", ctrl.GolangAssessmentCtrl.Question3).Methods("GET")
 }
