ddd7a238457459aeaaaeae86a49393931258dcdc
Zakiya Almaya <zakiya.almaya@gmail.com>
2024-05-09 22:50:07 +0700

    Delete duplicate folder & file

--- application/service.go
+++
@@ -1,7 +0,0 @@
-package application
-
-type Service interface {
-	Question1(nums []int, target int) []int 
-	Question2(nums []int) [][]int
-	Question3(str string, words []string) []int
-}
--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/zakiyalmaya/olin-assessment
-
-go 1.22.1
-
-require github.com/gorilla/mux v1.8.1
